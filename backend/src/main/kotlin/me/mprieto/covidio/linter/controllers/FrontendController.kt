e32cdec500f86e58163406b44fb943d2b54e1344
Miguel Prieto <hello@mprieto.me>
2020-07-08 11:51:48 -0400

    Very basic web panel with validation messages.

--- backend/src/main/kotlin/me/mprieto/covidio/linter/controllers/FrontendController.kt
+++ backend/src/main/kotlin/me/mprieto/covidio/linter/controllers/FrontendController.kt
@@ -1,12 +1,19 @@
 package me.mprieto.covidio.linter.controllers
 
+import me.mprieto.covidio.linter.services.atlassian.JiraCloudService
+import me.mprieto.covidio.linter.services.validators.ValidatorService
+import org.slf4j.Logger
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
 
+//TODO tests
 @Controller
-class FrontendController {
+class FrontendController(private val log: Logger,
+                         private val jiraService: JiraCloudService,
+                         private val validatorService: ValidatorService) {
 
     @GetMapping(value = ["/projects", "/projects/"])
     fun projects() = "redirect:/projects/index.html"
@@ -15,7 +22,14 @@ class FrontendController {
     fun issues() = "redirect:/issues/index.html"
 
     @GetMapping("/issue")
-    fun issue(@RequestParam("issue_key") issueKey: String): ModelAndView {
-        return ModelAndView("issue_web_panel", mapOf("issueKey" to issueKey))
+    fun issue(@RequestParam("issue_key") issueKey: String, model: Model): String {
+        val issue = jiraService.issue(issueKey)
+        log.debug("issue={}", issue)
+        model.addAttribute("issue", issue)
+        if (issue.isStory) {
+            val validation = validatorService.validate(issue.descriptionText)
+            model.addAttribute("validation", validation)
+        }
+        return "issue_web_panel"
     }
 }
