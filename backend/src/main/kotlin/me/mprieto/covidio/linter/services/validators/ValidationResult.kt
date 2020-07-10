fbe9975d167c5cd2246ecf2382aa437e607c0ec3
Miguel Prieto <hello@mprieto.me>
2020-07-09 23:49:37 -0400

    The enum Severity defines a type. No need to do Enum<Severity>.

--- backend/src/main/kotlin/me/mprieto/covidio/linter/services/validators/ValidationResult.kt
+++ backend/src/main/kotlin/me/mprieto/covidio/linter/services/validators/ValidationResult.kt
@@ -2,7 +2,7 @@ package me.mprieto.covidio.linter.services.validators
 
 enum class Severity { ERROR, SUCCESS, WARNING }
 
-data class ValidationResult(val isValid: Boolean, val severities: List<Enum<Severity>>, val messages: List<String>) {
-    constructor(isValid: Boolean, severity: Enum<Severity>) : this(isValid, listOf(severity), emptyList())
-    constructor(isValid: Boolean, severity: Enum<Severity>, message: String) : this(isValid, listOf(severity), listOf(message))
+data class ValidationResult(val isValid: Boolean, val severities: List<Severity>, val messages: List<String>) {
+    constructor(isValid: Boolean, severity: Severity) : this(isValid, listOf(severity), emptyList())
+    constructor(isValid: Boolean, severity: Severity, message: String) : this(isValid, listOf(severity), listOf(message))
 }
